83db7c8f525caf594af237dfe323eb897ce9d178
tsamantanis <philip.tsamantanis@icloud.com>
2021-09-13 04:25:11 -0700

    Process button

--- js/index.js
+++ js/index.js
@@ -7,12 +7,16 @@ var animateButton = function(e) {
     e.target.classList.add('animate');
     setTimeout(function(){
       e.target.classList.remove('animate');
-    }, 700);
+    }, 1200);
 };
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     let bubbleButton = document.getElementById("bubble-button");
     
     bubbleButton.addEventListener('click', animateButton, false);
+
+    let processButton = document.getElementById("process-button");
+    
+    processButton.addEventListener('click', animateButton, false);
 });
   
